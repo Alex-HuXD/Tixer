dfcfc1921328b45d10087626444b12c700b83a4c
XinDi Hu <HuXDev@pm.me>
2024-07-29 23:20:55 -0500

    setup for test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,40 +1,6 @@
 import 'express-async-errors'
-import express from 'express'
-import cookieSession from 'cookie-session'
-
 import mongoose from 'mongoose'
-
-import { currentUserRouter } from './routes/current-user'
-import { signinRouter } from './routes/signin'
-import { signoutRouter } from './routes/signout'
-import { signupRouter } from './routes/signup'
-import { errorHandler } from './middlewares/error-handler'
-import { BadRequestError } from './errors/bad-request-error'
-
-const app = express()
-app.set('trust proxy', true)
-
-app.use(express.json())
-app.use(
-    cookieSession({
-        signed: false,
-        secure: true,
-    })
-)
-
-// routes
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-app.use(signupRouter)
-
-// not found handler
-app.all('*', async () => {
-    throw new BadRequestError('page not found')
-})
-
-// error handler
-app.use(errorHandler)
+import { app } from './app'
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
